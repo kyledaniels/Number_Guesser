e5631e17c70ce1d4d81b4641a2408f3665f1fc14
kyle67 <daniels.kyle@gmail.com>
2020-08-12 00:45:50 -0400

    created function to generate number to guess

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+//Function that will generate number at start of game:
+
+const generateTarget = ()=>{
+   return Math.floor(Math.random()*10);
+}
