5ff712f6dd31c98c185b76f3e2cdf5867092984b
kyle67 <daniels.kyle@gmail.com>
2020-08-12 21:15:29 -0400

    created funtion to update score and round

--- script.js
+++ script.js
@@ -2,10 +2,36 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
 
-//Function that will generate number at start of game:
+//Function will generate new number for user and computer to guess:
 
 const generateTarget = ()=>{
-   return Math.floor(Math.random()*10);
+   target = Math.floor(Math.random()*10);
+   return target;
 }
+
+/*Function will determine which guess is closest to the target number.
+
+Determines which player (human or computer) wins based on which guess is closest to the target. 
+If both players are tied, the human user should win.
+Return true if the human player wins, and false if the computer player wins.*/
+
+const compareGuesses = (human, computer, target)=>{
+
+    if(human===target&&computer===target){
+        return 'You Win';
+    }if(Math.abs(target-human)< (Math.abs(target-computer)){
+        return true;
+    }else{
+        return false;
+    }
+}
+const updateScore = winner => {
+    if (winner === 'human') {
+      humanScore++;
+    } else if (winner === 'computer') {
+      computerScore++;
+    }
+  }
+  
+const advanceRound = () => currentRoundNumber++;
